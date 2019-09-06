807adef0750475446ddfb648f394b7dac31a58b5
Brett Jay Packard <brettjayp@gmail.com>
2019-09-05 19:08:18 -0700

    acp before starting new branch for new day

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// setting up some global variables
+var hoursOfOps = [];
+for (var i = 6; i <= 20; i++){
+  var hour = i + '00';
+  hoursOfOps.push(hour);
+}
+
+console.log(hoursOfOps);
