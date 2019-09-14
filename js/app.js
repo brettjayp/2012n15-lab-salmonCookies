362a884ff9968a3bd0d084bee4e4846007f61e6b
Brett Jay Packard <brettjayp@gmail.com>
2019-09-14 05:35:16 -0700

    table complete, but no table styling

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ var tableFoot = document.getElementById('tableFoot');
 
 // Global Variables
 var hoursOfOps = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-var dailyTotals = [];
+var totalTotal = 0;
 
 // Functions, other than constructor functions
 function getRandom(min, max){
@@ -33,7 +33,33 @@ function renderHead(){
   thEl.textContent = 'Total';
   tableHead.appendChild(thEl);
 }
-// function renderFoot(){};
+function renderFoot(){
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Hourly Totals';
+  tableFoot.appendChild(thEl);
+
+  hourlyTotals();
+  for(var i = 0; i < hoursOfOps.length; i++){
+    var tdEl = document.createElement('td');
+    tdEl.textContent = hoursTotals[i];
+    tableFoot.appendChild(tdEl);
+  }
+
+  thEl = document.createElement('th');
+  thEl.textContent = totalTotal;
+  tableFoot.appendChild(thEl);
+}
+var hoursTotals = [];
+function hourlyTotals(){
+  for(var i = 0; i < hoursOfOps.length; i++){
+    var hourlyTotal = 0;
+    for(var j = 0; j < allStores.length; j++){
+      hourlyTotal += allStores[j].cookiesPerHour[i];
+    }
+    totalTotal += hourlyTotal;
+    hoursTotals.push(hourlyTotal);
+  }
+}
 
 // Here's our constructor function. Below, method for calculating customers/hr and cookies/hr, and add to daily totals of 'this' store and dailyTotals. Last we render.
 function Store(name, min, max, avg){
@@ -52,7 +78,6 @@ Store.prototype.calculateCustomersAndCookies = function(){
     this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.averageCookiesPerSale));
     this.cookiesDailyTotal += this.cookiesPerHour[i];
   }
-  dailyTotals.push(this.cookiesDailyTotal);
 };
 
 Store.prototype.render = function(id){
@@ -80,6 +105,8 @@ var seattle = new Store('Seattle Center', 11, 38, 3.7);
 var capitol = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
+var allStores = [pike, seatac, seattle, capitol, alki];
+
 // Here we call our render methods.
 renderHead();
 pike.render(salesPike);
@@ -87,4 +114,4 @@ seatac.render(salesSeaTac);
 seattle.render(salesSeattle);
 capitol.render(salesCap);
 alki.render(salesAlki);
-// renderFoot();
+renderFoot();
