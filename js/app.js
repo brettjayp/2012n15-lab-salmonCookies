f4953a4828a7ed259f226a1a1ed39a6646efda0e
Brett Jay Packard <brettjayp@gmail.com>
2019-09-14 04:57:16 -0700

    converted to table, incomplete

--- js/app.js
+++ js/app.js
@@ -1,32 +1,47 @@
 /* eslint-disable no-unused-vars */
 'use strict';
 
-// getRandom function
-function getRandom(min, max){
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// hours of operations will be a global variable
-var hoursOfOps = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-var i = 0;
-
-// link each list with the DOM
+// required links with the DOM
+var tableHead = document.getElementById('tableHead');
 var salesPike = document.getElementById('salesPike');
 var salesSeaTac = document.getElementById('salesSeaTac');
 var salesSeattle = document.getElementById('salesSeattle');
 var salesCap = document.getElementById('salesCap');
 var salesAlki = document.getElementById('salesAlki');
+var tableFoot = document.getElementById('tableFoot');
 
-// here we'll begin to refactor, starting with pike. We'll begin by making it do the same thing, then convert to tables.
-var allStores = [];
+// Global Variables
+var hoursOfOps = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var dailyTotals = [];
 
-// Here we establish our constructor function. Below it, we set the method for calculating customers per hour and cookies per hour, and add to the daily total of 'this' store. Lastly, we render.
+// Functions, other than constructor functions
+function getRandom(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function renderHead(){
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Location';
+  tableHead.appendChild(thEl);
+
+  for(var i = 0; i < hoursOfOps.length; i++){
+    thEl = document.createElement('th');
+    thEl.textContent = hoursOfOps[i];
+    tableHead.appendChild(thEl);
+  }
+
+  thEl = document.createElement('th');
+  thEl.textContent = 'Total';
+  tableHead.appendChild(thEl);
+}
+// function renderFoot(){};
+
+// Here's our constructor function. Below, method for calculating customers/hr and cookies/hr, and add to daily totals of 'this' store and dailyTotals. Last we render.
 function Store(name, min, max, avg){
   this.storeName = name;
   this.minimumCustomers = min;
   this.maximumCustomers = max;
   this.averageCookiesPerSale = avg;
-};
+}
 
 Store.prototype.calculateCustomersAndCookies = function(){
   this.customersPerHour = [];
@@ -36,25 +51,26 @@ Store.prototype.calculateCustomersAndCookies = function(){
     this.customersPerHour.push(getRandom(this.minimumCustomers, this.maximumCustomers));
     this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.averageCookiesPerSale));
     this.cookiesDailyTotal += this.cookiesPerHour[i];
-  };
+  }
+  dailyTotals.push(this.cookiesDailyTotal);
 };
 
 Store.prototype.render = function(id){
   this.calculateCustomersAndCookies();
 
-  var lhEl = document.createElement('lh');
-  lhEl.textContent = this.storeName;
-  id.appendChild(lhEl);
+  var thEl = document.createElement('th');
+  thEl.textContent = this.storeName;
+  id.appendChild(thEl);
 
-  for(i = 0; i < hoursOfOps.length; i++){
-    var liEl = document.createElement('li');
-    liEl.textContent = `${hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`;
-    id.appendChild(liEl);
-  };
+  for(var i = 0; i < hoursOfOps.length; i++){
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.cookiesPerHour[i];
+    id.appendChild(tdEl);
+  }
 
-  liEl = document.createElement('li');
-  liEl.textContent = `Total: ${this.cookiesDailyTotal} cookies`;
-  id.appendChild(liEl);
+  thEl = document.createElement('th');
+  thEl.textContent = this.cookiesDailyTotal;
+  id.appendChild(thEl);
 };
 
 // Here we create our objects using our constructor function.
@@ -65,8 +81,10 @@ var capitol = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
 // Here we call our render methods.
+renderHead();
 pike.render(salesPike);
 seatac.render(salesSeaTac);
 seattle.render(salesSeattle);
 capitol.render(salesCap);
 alki.render(salesAlki);
+// renderFoot();
