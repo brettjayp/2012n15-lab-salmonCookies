d74c074bccffe7a497787b21f8eec1817466edaf
Brett Jay Packard <brettjayp@gmail.com>
2019-09-14 03:00:22 -0700

    created our constructor function and refactored all objects through it. ELIMINATED 200 LINES OF CODE

--- js/app.js
+++ js/app.js
@@ -17,235 +17,56 @@ var salesSeattle = document.getElementById('salesSeattle');
 var salesCap = document.getElementById('salesCap');
 var salesAlki = document.getElementById('salesAlki');
 
-// create each site and populate it's data
-var pike = {
-  name: 'First and Pike',
-  custMin: 23,
-  custMax: 65,
-  avgCookiesPerSale: 6.3,
-
-  customersPerHour: [],
-  cookiesPerHour: [],
-  cookiesDailyTotal: 0,
-
-  calcCustomerAndCookies: function(){
-    for(var i = 0; i < hoursOfOps.length; i++){
-      this.customersPerHour.push(getRandom(this.custMin, this.custMax));
-      this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerSale));
-      this.cookiesDailyTotal += this.cookiesPerHour[i];
-    }
-  },
-
-  render: function(){
-    this.calcCustomerAndCookies();
-
-    var lhEl = document.createElement('lh');
-    lhEl.textContent = this.name;
-    salesPike.appendChild(lhEl);
-
-    for(i = 0; i < hoursOfOps.length; i++){
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`;
-      salesPike.appendChild(liEl);
-    }
-
-    liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.cookiesDailyTotal} cookies`;
-    salesPike.appendChild(liEl);
-  },
-};
-
 // here we'll begin to refactor, starting with pike. We'll begin by making it do the same thing, then convert to tables.
-var locations = [];
+var allStores = [];
 
-// function addStore(name, custMin, custMax, avgCookies){
-//   this.name = name;
-//   this.custMin = custMin;
-//   this.custMax = custMax;
-//   this.avgCookiesPerSale = avgCookies;
-//   this.customersPerHour = [];
-//   this.cookiesPerHour = [];
-//   this.cookiesDailyTotal = 0;
-
-//   locations.push(this);
-// }
-
-// addStore.prototype.calcCustomersPerHour = function(){
-//   for(i = 0; i < hoursOfOps.length; i++){
-//     this.customersPerHour.push(getRandom(this.custMin,this.custMax));
-//   }
-// };
-
-// addStore.prototype.calcCookiesPerHour = function(){
-//   for(i = 0; i < hoursOfOps.length; i++){
-//     this.cookiesPerHour.push(Math.floor(this.customersPerHour[i]))
-//   }
-// };
-
-
-// cookiesPerHour: [],
-// cookiesDailyTotal: 0,
-// calcCookiesPerHour: function(){
-//   for(i = 0; i < hoursOfOps.length; i++){
-//     this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerSale)); // Using Math.floor to return whole integers instead of long trailing decimal values
-//     this.cookiesDailyTotal += this.cookiesPerHour[i]; // Doing this here prevents unnecessary code
-//   }
-// },
-
-// here we'll test it on pike
-// addStore('First and Pike', 23, 65, 6.3);
-
-
-
-var seatac = {
-  name: 'SeaTac Airport',
-  custMin: 3,
-  custMax: 24,
-  avgCookiesPerSale: 1.2,
-
-  customersPerHour: [],
-  cookiesPerHour: [],
-  cookiesDailyTotal: 0,
-
-  calcCustomerAndCookies: function(){
-    for(var i = 0; i < hoursOfOps.length; i++){
-      this.customersPerHour.push(getRandom(this.custMin, this.custMax));
-      this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerSale));
-      this.cookiesDailyTotal += this.cookiesPerHour[i];
-    }
-  },
-
-  render: function(){
-    this.calcCustomerAndCookies();
-
-    var lhEl = document.createElement('lh');
-    lhEl.textContent = this.name;
-    salesSeaTac.appendChild(lhEl);
-
-    for(i = 0; i < hoursOfOps.length; i++){
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`;
-      salesSeaTac.appendChild(liEl);
-    }
-
-    liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.cookiesDailyTotal} cookies`;
-    salesSeaTac.appendChild(liEl);
-  },
-};
-var seattle = {
-  name: 'Seattle Center',
-  custMin: 11,
-  custMax: 38,
-  avgCookiesPerSale: 3.7,
-
-  customersPerHour: [],
-  cookiesPerHour: [],
-  cookiesDailyTotal: 0,
-
-  calcCustomerAndCookies: function(){
-    for(var i = 0; i < hoursOfOps.length; i++){
-      this.customersPerHour.push(getRandom(this.custMin, this.custMax));
-      this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerSale));
-      this.cookiesDailyTotal += this.cookiesPerHour[i];
-    }
-  },
-
-  render: function(){
-    this.calcCustomerAndCookies();
-
-    var lhEl = document.createElement('lh');
-    lhEl.textContent = this.name;
-    salesSeattle.appendChild(lhEl);
-
-    for(i = 0; i < hoursOfOps.length; i++){
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`;
-      salesSeattle.appendChild(liEl);
-    }
-
-    liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.cookiesDailyTotal} cookies`;
-    salesSeattle.appendChild(liEl);
-  },
-};
-var capitol = {
-  name: 'Capitol Hill',
-  custMin: 20,
-  custMax: 38,
-  avgCookiesPerSale: 2.3,
-
-  customersPerHour: [],
-  cookiesPerHour: [],
-  cookiesDailyTotal: 0,
-
-  calcCustomerAndCookies: function(){
-    for(var i = 0; i < hoursOfOps.length; i++){
-      this.customersPerHour.push(getRandom(this.custMin, this.custMax));
-      this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerSale));
-      this.cookiesDailyTotal += this.cookiesPerHour[i];
-    }
-  },
-
-  render: function(){
-    this.calcCustomerAndCookies();
-
-    var lhEl = document.createElement('lh');
-    lhEl.textContent = this.name;
-    salesCap.appendChild(lhEl);
-
-    for(i = 0; i < hoursOfOps.length; i++){
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`;
-      salesCap.appendChild(liEl);
-    }
-
-    liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.cookiesDailyTotal} cookies`;
-    salesCap.appendChild(liEl);
-  },
-};
-var alki = {
-  name: 'Alki',
-  custMin: 2,
-  custMax: 16,
-  avgCookiesPerSale: 4.6,
-
-  customersPerHour: [],
-  cookiesPerHour: [],
-  cookiesDailyTotal: 0,
-
-  calcCustomerAndCookies: function(){
-    for(var i = 0; i < hoursOfOps.length; i++){
-      this.customersPerHour.push(getRandom(this.custMin, this.custMax));
-      this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerSale));
-      this.cookiesDailyTotal += this.cookiesPerHour[i];
-    }
-  },
-
-  render: function(){
-    this.calcCustomerAndCookies();
-
-    var lhEl = document.createElement('lh');
-    lhEl.textContent = this.name;
-    salesAlki.appendChild(lhEl);
-
-    for(i = 0; i < hoursOfOps.length; i++){
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`;
-      salesAlki.appendChild(liEl);
-    }
-
-    liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.cookiesDailyTotal} cookies`;
-    salesAlki.appendChild(liEl);
-  },
+// Here we establish our constructor function. Below it, we set the method for calculating customers per hour and cookies per hour, and add to the daily total of 'this' store. Lastly, we render.
+function Store(name, min, max, avg){
+  this.storeName = name;
+  this.minimumCustomers = min;
+  this.maximumCustomers = max;
+  this.averageCookiesPerSale = avg;
 };
 
-// render each site location
-pike.render();
-seatac.render();
-seattle.render();
-capitol.render();
-alki.render();
+Store.prototype.calculateCustomersAndCookies = function(){
+  this.customersPerHour = [];
+  this.cookiesPerHour = [];
+  this.cookiesDailyTotal = 0;
+  for(var i = 0; i < hoursOfOps.length; i++){
+    this.customersPerHour.push(getRandom(this.minimumCustomers, this.maximumCustomers));
+    this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.averageCookiesPerSale));
+    this.cookiesDailyTotal += this.cookiesPerHour[i];
+  };
+};
 
+Store.prototype.render = function(id){
+  this.calculateCustomersAndCookies();
+
+  var lhEl = document.createElement('lh');
+  lhEl.textContent = this.storeName;
+  id.appendChild(lhEl);
+
+  for(i = 0; i < hoursOfOps.length; i++){
+    var liEl = document.createElement('li');
+    liEl.textContent = `${hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`;
+    id.appendChild(liEl);
+  };
+
+  liEl = document.createElement('li');
+  liEl.textContent = `Total: ${this.cookiesDailyTotal} cookies`;
+  id.appendChild(liEl);
+};
+
+// Here we create our objects using our constructor function.
+var pike = new Store('First and Pike', 23, 65, 6.3);
+var seatac = new Store('SeaTac Airport', 3, 24, 1.2);
+var seattle = new Store('Seattle Center', 11, 38, 3.7);
+var capitol = new Store('Capitol Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
+
+// Here we call our render methods.
+pike.render(salesPike);
+seatac.render(salesSeaTac);
+seattle.render(salesSeattle);
+capitol.render(salesCap);
+alki.render(salesAlki);
